e8249976c713f8146f6937606e3820bc7c279f1d
seok10code <seok10code@gmail.com>
2024-07-04 17:30:33 +0900

    2024-07-04

---
+++ leetcode_sql/medium/626. Exchange Seats.sql
@@ -0,0 +1,9 @@
+-- # Write your MySQL query statement below
+select 
+case
+when id%2=1 and id=(select max(id) from seat) then id
+when id%2=1 then id+1
+when id%2=0 then id-1 end id ,
+student
+ from seat
+ order by id
